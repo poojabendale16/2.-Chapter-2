0576716e9ccbae6157215c36386b216b1ed46234
poojabendale16 <poojabendale2012@gmail.com>
2021-04-20 16:08:46 +0530

    with comments

---
+++ add_remainder_type_avg.py
@@ -0,0 +1,9 @@
+a=input("Enter first number:")
+b=input("Enter second number:")
+a=int(a)
+b=int(b)
+c = a+b
+print("the sum: ", a + b )
+print("remainder", a % b )
+print("avg",c/2 )
+print(type(a))
